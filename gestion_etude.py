9fdea0aca096135611c01ca225d61787a9fd1558
Ahmed Azarki <ahmedmohamedazarki@gmail.com>
2026-05-02 15:33:07 +0100

    add menu & display func

--- gestion_etude.py
+++ gestion_etude.py
@@ -3,3 +3,22 @@ note=[[12,10,20],[20,15,17],[19,18,9]]
 age=[14,15,14]
 
 
+def afficher():
+    for i in etudien:
+        print(i)
+
+
+def menu():
+    while True:
+        print("1:ajouter etudien")
+        print("2:ajouter ")
+        print("5 : quiter")
+
+        n = input("donner un nombre de menu")
+        if n =="1":
+            afficher()
+        elif n =="5":
+            return False
+        else : 
+            print("unvalide nombre")
+menu()
